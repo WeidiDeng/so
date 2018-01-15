130b88098f4d3173ae14069cfa86af3909603937
luoye <luoyefe@gmail.com>
2018-01-15 13:53:50 +0800

    update: 封装下 request

--- index.js
+++ index.js
@@ -6,29 +6,25 @@ const request = require('request');
 const port = 9901;
 const homeHTML = fs.readFileSync('./home.html', 'utf-8');
 
+function requestInstance(url) {
+	return request({
+		url,
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+		}
+	})
+}
+
 const Server = http.createServer((req, res) => {
 	const url = URL.parse(req.url, true);
 	if (url.pathname === '/') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write(homeHTML);
-		res.end();
+		req.pipe(requestInstance('https://www.google.com')).pipe(res);
 	} else if (url.pathname === '/search'){
-		console.log(url);
 		// 代理搜索结果
-		req.pipe(request({
-			url: `https://www.google.com/search${url.search}`,
-			headers: {
-				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
-			}
-		})).pipe(res);
+		req.pipe(requestInstance(`https://www.google.com/search${url.search}`)).pipe(res);
 	} else {
-		// 代理其他 google.com 下的请求
-		req.pipe(request({
-			url: `https://www.google.com/${url.path}`,
-			headers: {
-				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
-			}
-		})).pipe(res);
+		// 代理其他请求道 google.com 下
+		req.pipe(requestInstance(`https://www.google.com/${url.path}`)).pipe(res);
 	}
 });
 
